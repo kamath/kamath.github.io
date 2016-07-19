3b9800564bd9a6912fe2abaa5c8d4c65a1f74401
Andy Kamath <andy@andykamath.com>
2016-07-19 18:24:24 +0530

    major design overhaul lmao

--- README.md
+++
@@ -1,3 +0,0 @@
-#My Personal Website
-
-I like to do stuff :)
--- _site/andykamath.github.io/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-$(".navitem").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
-
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
